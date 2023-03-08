89c92b893e9b87f06ac227348cac42558c0c7f42
Nate Brown <nathan.d.brown@gmail.com>
2023-03-07 22:02:28 -0800

    city info displays

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       cityName: '',
-      cityData: {},
+      cityData: [],
       error: false,
       errorMessage: ''
     }
@@ -29,32 +29,56 @@ class App extends React.Component {
   citySubmit = async (event) => {
     event.preventDefault();
     console.log(this.state.cityName);
-    let cityData = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.cityName}&format=json`);
-    
-    // let cityData = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.cityName}&format=json`);
+    let cityResults = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.cityName}&format=json`);
 
-    
-    console.log(cityData.data[0]);
+    this.setState({
+      cityData: cityResults.data
+    });
+
+    console.log(this.state.cityData);
+    console.log(cityResults.data[0]);
   }
 
   render() {
+
+    let cityNamesList = this.state.cityData.map((cityData, index) => {
+      return <li key={index}>{cityData.display_name}</li>
+    })
+
+    let cityLatList = this.state.cityData.map((cityData, index) => {
+      return <li key={index}>{cityData.lat}</li>
+    })
+
+    let cityLonList = this.state.cityData.map((cityData, index) => {
+      return <li key={index}>{cityData.lon}</li>
+    })
+
+    // let mapURL = ''
+
+
+    console.log(this.state.cityData);
+
     return (
       <>
         <Header/>
         {/* <Main/> */}
         <>
-        <main>
-          <h2>Main</h2>
+        <main className="mainClass">
+          <div>
+          <h2>Enter a Place in the Form</h2>
           <div className="mainForm">
           <Form onSubmit={this.citySubmit}>
             <Form.Label>Pick a City
               <Form.Control type="text" onChange={this.handleCityInput}/>
             </Form.Label>
-            <Button type="submit">Get City Data</Button>
+            <Button type="submit">Explore!</Button>
           </Form>
           </div>
+          </div>
           <div className="results">
-            {this.props.cityData}
+            <p>{cityNamesList[0]}</p>
+            <p>{cityLatList[0]}</p>
+            <p>{cityLonList[0]}</p>
           </div>
         </main>
       </>
--- src/Main.js
+++ src/Main.js
@@ -13,7 +13,7 @@ class Main extends React.Component {
           <h2>Main</h2>
           <div className="mainForm">
           <Form onSubmit={this.props.citySubmit}>
-            <Form.Label>Pick a City
+            <Form.Label>Explore!
               <Form.Control type="text" onChange={this.props.handleCityInput} name="city"/>
             </Form.Label>
             <Button type="submit">Submit</Button>
