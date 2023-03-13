c8ab880d6cc0bf2454fc3e73cfe5fe482b427842
Nate Brown <nathan.d.brown@gmail.com>
2023-03-12 22:26:12 -0700

    map working, map request error reporting working, map error hides after correct request

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Main.css';
 import { Form } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 class Main extends React.Component {
 
@@ -9,22 +10,37 @@ class Main extends React.Component {
 
     return (
       <>
-        <main className="mainClass">
-          <h2>Main</h2>
-          <div className="mainForm">
-          <Form onSubmit={this.props.citySubmit}>
-            <Form.Label>Explore!
-              <Form.Control type="text" onChange={this.props.handleCityInput} name="city"/>
-            </Form.Label>
-            <Button type="submit">Submit</Button>
-          </Form>
-          </div>
-          <div className="results">
-            {this.props.cityData}
-          </div>
-        </main>
+          <main className="mainClass">
+            <div className="mainDiv">
+              <h2>Enter a Place in the Form</h2>
+              <div>
+                <Form onSubmit={this.props.handleCitySubmit}>
+                  <Form.Label>Pick a City
+                    <Form.Control type="text" onChange={this.props.handleCityInput} />
+                  </Form.Label>
+                  <Button type="submit">Explore!</Button>
+                </Form>
+              </div>
+            </div>
+            <div className="mainDiv">
+              <Card id="cityResultsCard" className="resultsCardClass">
+                {/* {cityNamesList[0]} */}
+              </Card>
+              <Card id="cityMapCard" className="resultsCardClass">
+                {/* <img src={mapURL} alt='map of selected city' /> */}
+                {/* <Weather/> */}
+              </Card>
+              <Card id="cityWeatherCard" className="resultsCardClass">
+                {/* {cityForecast} */}
+              </Card>
+            </div>
+            <div className="mainDiv">
+              <ul>
+              </ul>
+            </div>
+          </main>
       </>
-    )
+    );
 
   }
 }
